95d463d70b5fc88e92f44b75bd5a4e121993fa82
ryan010417 <117926375+ryan010417@users.noreply.github.com>
2023-05-21 22:16:58 +0800

    Create a rock paper scissors game

---
+++ javascript.js
@@ -0,0 +1,30 @@
+function computerPlay() {
+    let rps = ["rock", "paper", "scissors"];
+    let random = rps[Math.floor(Math.random() * rps.length)];
+    return random;
+}
+
+console.log(computerPlay());
+
+function playRound (playerSelection,computerSelection){
+    if (playerSelection === computerSelection) {
+        return ("It's a draw");
+    }   else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
+        return ("You win! Rock beats scissors");
+      } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return ("You lose! Paper beats rock");
+      } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return ("You win! Paper beats rock");
+      } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return ("You lose! Scissors beat paper");
+      } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return ("You win! Scissors beat paper");
+      } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return ("You lose!Rock beats scissors");
+      }
+}
+
+let computerSelection = computerPlay();
+let playerSelection = prompt("Rock, Paper, Scissors");
+console.log(computerSelection)
+console.log(playRound(playerSelection,computerSelection));
